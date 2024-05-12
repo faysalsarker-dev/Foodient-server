ac1819afba5341eeb6c018a2e1cfa9f6a92a3dac
Faysal-sarker <fridaysharker100@gmail.com>
2024-05-12 12:32:25 +0600

    deploy versel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://server-teal-phi.vercel.app"],
     credentials: true,
   })
 );
@@ -41,23 +41,42 @@ const client = new MongoClient(uri, {
   },
 });
 
+const veryfyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return req.status(401).send({ Message: "unauthoraize acces " });
+  }
+  jwt.verify(token, process.env.SECRET_SERVER, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthoraize acces" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     const foodCollection = client.db("food_collection").collection("food");
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.DB_SECRET, { expiresIn: "1h" });
+      const token = jwt.sign(user, process.env.DB_SECRET);
       res
         .cookie("token", token, {
           httpOnly: true,
-          //   secure: true,
-          //   sameSite: none,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
 
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
     app.post("/addfood", async (req, res) => {
       const data = req.body;
       console.log(data);
@@ -70,17 +89,61 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/food/:id',async(req,res)=>{
+    app.get("/food/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await foodCollection.findOne(query).toArray()
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/manage-my-foods/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { Doner_email: email };
+      const result = await foodCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.patch("/food-request/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const data = req.body;
+      const info = {
+        $set: {
+          name: data.name,
+          img: data.img,
+          FoodQuantity: data.FoodQuantity,
+          PickupLocation: data.PickupLocation,
+          AdditionalNotes: data.AdditionalNotes,
+          expiredate: data.expiredate,
+          Doner_name: data.Doner_name,
+          Doner_email: data.Doner_email,
+          Status: data.Status,
+          requestor_email: data.requestor_email,
+          requestor_name: data.requestor_name,
+        },
+      };
+      console.log(info);
+      const result = await foodCollection.updateOne(query, info, options);
+      res.send(result);
+    });
 
     app.get("/Available-Foods", async (req, res) => {
-      const query = {Status:'available'}
-      const result = await foodCollection.find(query).toArray()
-      res.send(result)
+      const search = req.query.search;
+      const sort = req.query.sort;
+
+      const query = { Status: "available" };
+      if (search) {
+        query.name = { $regex: search, $options: "i" };
+      }
+
+      let option = {};
+
+      if (sort) {
+        option = { sort: { expiredate: sort === "ascending" ? 1 : -1 } };
+      }
+      const result = await foodCollection.find(query, option).toArray();
+      res.send(result);
     });
 
     console.log(
